5aa698a5e6f8720310048536b6996f170ccec84a
sola <solaocodes@gmail.com>
2019-06-15 16:19:50 -0400

    Use react router and create routes to content pages.

--- src/Main.js
+++ src/Main.js
@@ -1,17 +1,32 @@
 import React, { Component } from "react";
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Countdown from "./Countdown";
+import Future from "./Future";
+import About from "./About";
+
 
 class Main extends Component {
   render() {
     return (
-      <div>
-        <h1>Space X</h1>
-        <ul className="header">
-          <li><a href="/">Countdown</a></li>
-          <li><a href="/future">Future</a></li>
-          <li><a href="/About">About</a></li>
-        </ul>
-        <div className="content"></div>
-      </div>
+      <HashRouter>
+        <div>
+          <h1>Space X</h1>
+          <ul className="header">
+            <li><NavLink to="/">Countdown</NavLink></li>
+            <li><NavLink to="/future">Future</NavLink></li>
+            <li><NavLink to="/about">About</NavLink></li>
+          </ul>
+          <div className="content">
+            <Route exact path="/" component={Countdown}/>
+            <Route path="/future" component={Future}/>
+            <Route path="/about" component={About}/>
+          </div>
+        </div>
+      </HashRouter>
     );
   }
 }
