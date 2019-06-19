14a6341c4581c31b4d7042cf10e4f36b348e6256
sola <solaocodes@gmail.com>
2019-06-18 22:07:39 -0400

    Display and style data in Future Component.

--- src/Future.js
+++ src/Future.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import Row from 'react-bootstrap/Row';
+import Table from 'react-bootstrap/Table'
 
 const SpacexApiWrapper = require("spacex-api-wrapper");
 
@@ -7,26 +9,61 @@ class Future extends Component {
     super(props, context);
 
     this.state = {
-      data: ""
+      data: []
     };
     this.getData = this.getData.bind(this);
   }
 
   componentDidMount() {
-    // this.getData();
+    this.getData();
+  }
+
+  renderLaunchData() {
+    if (this.state.data.length > 0) {
+      return this.state.data.map((launch, index) => {
+        const flightNumber = launch.flight_number;
+        const launchDate = (new Date(launch.launch_date_utc)).toLocaleDateString();
+        const siteName = launch.launch_site.site_name_long;
+        const rocketName = launch.rocket.rocket_name;
+        const missionName = launch.mission_name;
+
+        return (
+          <tr key={flightNumber}>
+            <td>{flightNumber}</td>
+            <td>{launchDate}</td>
+            <td>{rocketName}</td>
+            <td>{missionName}</td>
+            <td className="site-name">{siteName}</td>
+          </tr>
+        );
+      });
+    }
   }
 
   getData() {
-    SpacexApiWrapper.getUpcomingLaunches({limit: 5}).then((data) => {
-      this.setState({
-        data: data
-      });
-    });
+    SpacexApiWrapper.getUpcomingLaunches()
+      .then((data) => this.setState({ data: data }));
   }
 
   render() {
     return (
-      <h1>This will be the <strong>Future</strong> page</h1>
+      <Row>
+      <h1 className="mx-auto">Next Launches</h1>
+        <Table borderless hover responsive className="next-launches">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Launch Date</th>
+              <th>Rocket</th>
+              <th>Mission Name</th>
+              <th>Launch Site</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.renderLaunchData()}
+          </tbody>
+        </Table>
+      </Row>
     );
   }
 }
